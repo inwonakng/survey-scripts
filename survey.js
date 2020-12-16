c6417fcaaf08fcc49a466a89d016648d50cfd54a
inwonakng <inwon.kang04@gmail.com>
2020-12-15 22:50:12 -0500

    some styling stuff

--- survey.js
+++ survey.js
@@ -15,9 +15,35 @@ $(document).ready(()=>{
         header.append(th)
         body.insertCell().innerHTML=entities[ent]
     }
-    $('#entities-table').append(table)
+    $('#entities-labels').append(table)
     //finished with table here 
     
+
+    // filling out the entity information table
+    
+    table = document.createElement('table')
+    table.className = 'info-table'
+    header = table.insertRow()
+    header.insertCell()
+    
+    for(ent in entity_values){
+        th = document.createElement('th')
+        th.innerHTML=ent
+        header.append(th)
+    }
+
+    for(param in entity_values[ent]){
+        body = table.insertRow()
+        body.insertCell().innerHTML=param
+        for(ee in entity_values){
+            body.insertCell().innerHTML = entity_values[ee][param]
+        }
+    }
+
+    $('#info-section').append(table)
+
+
+
     $('#show-more').on('click',event=>{
         count = $('#context-container .context-block').length
         next_context = ref_tree[main_idx][count]
