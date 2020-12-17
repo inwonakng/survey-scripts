42a2a537c618577a7614c165e2d362fdbf08786c
inwonakng <inwon.kang04@gmail.com>
2020-12-16 23:49:45 -0500

    drag and drop experiment

--- survey.js
+++ survey.js
@@ -1,8 +1,18 @@
+// javascript doesn't have a function to remove one element from list...
+function remove(arr,ele){
+    idx = arr.indexOf(ele)
+    arr.splice(idx,1)
+    return arr
+}
+
 $(document).ready(()=>{
     // setting the first visible comment
     main_idx = $('#comment-idx')[0].value
     text = comments[main_idx]
     $('#main-text')[0].innerHTML=text
+
+    // setting the original comment
+    $('#original-comment')[0].innerHTML = comments[0]
 
     // creating table to explain entities
     table = document.createElement('table')
@@ -48,7 +58,7 @@ $(document).ready(()=>{
         count = $('#context-container .context-block').length
         next_context = ref_tree[main_idx][count]
 
-        innertext =  '<p class="context-block">'
+        innertext =  '<p class="context-block text-block">'
         innertext += comments[next_context]
         innertext += '</p>'
         $('#context-container').prepend(
@@ -66,4 +76,43 @@ $(document).ready(()=>{
             event.target.disabled = true
         }
     })
+
+    // setting the initial value here
+    lastbox = $('#dragbox').children().last()[0] 
+    lastinp = $(lastbox).find('input')[0]
+    vals = []
+    
+    for (i of $(lastbox).find('li.drag-box')){
+        vals.push(i.innerHTML)
+    }
+
+    lastinp.value = JSON.stringify(vals)
+
+    //dragstart event to initiate mouse dragging
+    document.addEventListener('dragstart', function(e)
+    {
+        dragged = e.target
+        inp = $(e.target).parent().find('input')[0]
+        vals = JSON.parse(inp.value)
+        inp.value = JSON.stringify(remove(vals,e.target.innerHTML))
+    },false);
+
+    document.addEventListener('dragover', function(e){
+        if(e.target){
+            e.preventDefault();
+        }
+    },false);  
+
+    //drop event to allow the element to be dropped into valid targets
+    document.addEventListener('drop', function(e)
+    {
+        if(e.target.getAttribute('data-draggable') == 'target'){
+            e.target.appendChild(dragged);
+            inp = $(e.target).find('input')[0]
+            vals = JSON.parse(inp.value)
+            // console.log('got value',vals)
+            vals.push(dragged.innerHTML)
+            inp.value = JSON.stringify(vals)
+        }
+    },false);
 })
